e147bedd2908f56bf05081bb42a4e41f807e7261
0xshoegazer <0xshoegazer@gmail.com>
2023-08-26 14:06:08 -0400

    fees

--- contracts/UniswapV3Factory.sol
+++ contracts/UniswapV3Factory.sol
@@ -23,12 +23,12 @@ contract UniswapV3Factory is IUniswapV3Factory, UniswapV3PoolDeployer, NoDelegat
         owner = msg.sender;
         emit OwnerChanged(address(0), msg.sender);
 
-        feeAmountTickSpacing[100] = 1;
-        emit FeeAmountEnabled(100, 1);
-        feeAmountTickSpacing[500] = 10;
-        emit FeeAmountEnabled(500, 10);
-        feeAmountTickSpacing[3000] = 60;
-        emit FeeAmountEnabled(3000, 60);
+        feeAmountTickSpacing[80] = 1;
+        emit FeeAmountEnabled(80, 1);
+        feeAmountTickSpacing[450] = 10;
+        emit FeeAmountEnabled(450, 10);
+        feeAmountTickSpacing[2500] = 60;
+        emit FeeAmountEnabled(2500, 60);
         feeAmountTickSpacing[10000] = 200;
         emit FeeAmountEnabled(10000, 200);
     }
